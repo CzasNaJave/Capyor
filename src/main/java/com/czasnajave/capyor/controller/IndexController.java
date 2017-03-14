fc71aee8bcfe8af73a3881d70f0c551f6eb94630
CzasNaJave <czasnajave@gmail.com>
2017-03-14 12:41:26 +0100

    Update IndexController.java

--- src/main/java/com/czasnajave/capyor/controller/IndexController.java
+++ src/main/java/com/czasnajave/capyor/controller/IndexController.java
@@ -3,12 +3,7 @@ package com.czasnajave.capyor.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created with IntelliJ IDEA.
- * User: czasnajave
- * Date: 12/20/12
- * Time: 5:27 PM
- */
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
