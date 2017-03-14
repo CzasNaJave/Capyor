0fca5fef234ea20fc578d481aa31f8f909992f42
CzasNaJave <czasnajave@gmail.com>
2017-03-14 13:00:39 +0100

    Update UserController.java

--- src/main/java/com/czasnajave/capyor/controller/UserController.java
+++ src/main/java/com/czasnajave/capyor/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
 
 
     @RequestMapping("/layout")
-    public String getUserPartialPage(ModelMap modelMap) {
+    public String getUserPartialPage() {
         return "users/layout";
     }
 }
