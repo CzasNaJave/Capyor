905d2ac841e5372da13a749960f6ba4f40a0257e
CzasNaJave <czasnajave@gmail.com>
2017-03-14 12:41:45 +0100

    Update ImageService.java

--- src/main/java/com/czasnajave/capyor/service/ImageService.java
+++ src/main/java/com/czasnajave/capyor/service/ImageService.java
@@ -2,9 +2,6 @@ package com.czasnajave.capyor.service;
 
 import com.czasnajave.capyor.beans.Image;
 
-/**
- * Created by Exilion on 2017-02-08.
- */
 
 public interface ImageService {
     public void addImage(Image image);
