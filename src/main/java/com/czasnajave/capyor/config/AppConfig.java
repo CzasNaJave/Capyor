cccda35e1c866edd6239f15b837fb9972907185c
CzasNaJave <czasnajave@gmail.com>
2017-03-14 12:40:05 +0100

    Update AppConfig.java

--- src/main/java/com/czasnajave/capyor/config/AppConfig.java
+++ src/main/java/com/czasnajave/capyor/config/AppConfig.java
@@ -2,9 +2,7 @@ package com.czasnajave.capyor.config;
 
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Created by czasnajave on 12/24/15.
- */
+
 
 @Configuration
 public class AppConfig{
